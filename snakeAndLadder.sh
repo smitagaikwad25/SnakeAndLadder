1e17cbc388a9a28551112b3af0a70b269264baf5
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-26 16:50:58 +0530

    Add : Logic to check position of player

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -8,6 +8,18 @@ position=0
 function  funnyGame()
 	{
 		die=$((RANDOM%6+1))
+		random=$((RANDOM%3+1))
+		case $random in
+			1)
+				position=$position
+				echo "NO Play" ;;
+			2)
+				position=$(( $position+$die ))
+				echo "position of player $position" ;;
+			3)
+				position=$(( $position-$die )) 
+				echo "position after snake $position" ;;
+		esac
 
 	}
 funnyGame
